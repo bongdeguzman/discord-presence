819386f8dc24463d9f4a64c295ad6e6fefe7681d
Bong DeGuzman <79473084+BDevelopment1@users.noreply.github.com>
2023-03-22 18:11:01 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config.VIPList = {
+	'steam:' -- Example, change this
+}
+
+Config.VIP2List = {
+	'steam:' -- Example, change this
+}
+
+Config.VIP3List = {
+   'steam:', -- Example, change this
+}
+
+Config.Admin = {
+   'steam:', -- Example, change this
+}
